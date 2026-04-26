36abcd1ee925cc7d72b2fcefcc85cac24038c327
哇哈哈补钙 <3575414734@qq.com>
2026-04-26 20:45:14 +0800

    add python day03

---
+++ python-basics/day03/compare_age.py
@@ -0,0 +1,8 @@
+age = int(input("please input your age: "))
+
+if age > 18:
+    print("you are older than 18")
+elif age == 18:
+    print("you are exactly 18")
+else:
+    print("you are younger than 18")
